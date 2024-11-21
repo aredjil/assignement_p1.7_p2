9d3c1a547d5c065872d3bfc400503627407b68ca
bellou <rand@random.it>
2024-11-21 16:05:30 +0100

    handle the commandline

--- src/script.py
+++ src/script.py
@@ -1,1 +1,7 @@
-print("Hello Beautiful world")
+import matplotlib.pyplot as plt 
+import sys 
+
+# Handling the command line  
+assert int(len(sys.argv)) != 1 and int(len(sys.argv)) <= 2, "program needs one argument as the mode"
+# Getting the mode from the user
+mode = int(sys.argv[1])
